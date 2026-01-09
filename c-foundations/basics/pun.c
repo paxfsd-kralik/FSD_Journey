e9c2fd47fab181c42fa8da6b4a2510732be8d7a1
Jindra Kralik <pax.fsd@gmail.com>
2026-01-09 13:19:49 +0100

    First program created and tested: The bad pun.

---
+++ c-foundations/basics/pun.c
@@ -0,0 +1,13 @@
+/*	Name: pun.c									*/
+/*	Purpose: Prints a bad pun.					*/
+/*	Author: Jindra Kralik						*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("To C or not to C: ");
+	printf("that is the question.\n");
+	printf("Brevity is the soul of wit. \n --Shakespeare\n");
+	return 0;
+}
