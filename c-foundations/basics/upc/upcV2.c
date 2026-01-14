a88ed9d2a0c4ff357baced786a03a3a802e92c80
Jindra Kralik <pax.fsd@gmail.com>
2026-01-14 17:15:05 +0100

    Starting chapter's 5 excercises about selection statements

--- c-foundations/basics/upc/upcV2.c
+++ c-foundations/basics/upc/upcV2.c
@@ -1,5 +1,5 @@
 /*
-Name: upc.ca 
+Name: upcV2.ca 
 Purpose: Universal Product Code check digit
 Author: Jindra Kralik
 */
@@ -10,13 +10,9 @@ int	main(void)
 {
 	int	d, i1, i2, i3, i4, i5, j1, j2, j3, j4, j5, first_sum, second_sum, total;
 
-	printf("Enter the first single digit: ");
-	scanf("%1d", &d);
-	printf("Enter the first group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &i1, &i2, &i3, &i4, &i5);
-	printf("Enter the second group of five digits: ");
-	scanf("%1d%1d%1d%1d%1d", &j1, &j2, &j3, &j4, &j5);
-
+	printf("Enter the first eleven digits of a UPC: ");
+	scanf("%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d%1d", &d, &i1, &i2, &i3, &i4, &i5, &j1, &j2, &j3, &j4, &j5);
+	
 	first_sum = d + i2 + i4 + j1 + j3 + j5;
 	second_sum = i1 + i3 + i5 + j2 + j4;
 	total = 3 * first_sum + second_sum;
